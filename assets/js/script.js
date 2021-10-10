4995b316540a4a0aa258f0594f236ef78cf75fc0
Cosdaman <Cosdaguy@gmail.com>
2021-10-10 12:22:10 -0700

    cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,39 +10,52 @@ var container = $('.container');
 $('#currentDay').text(currentDayDate);
 
 
-//time block creation
-for (let i = 9; i < 18; i++) {
-    var timeblockHour = moment().startOf('day').hour(i)
-    var hourFormat = moment(timeblockHour).format('h:mm a')
-    var divElRow = $('<div>');
-    var divElHour = $('<div>')
-    var divElText = $('<div>');
-    var divElSave = $('<div>');
-    divElRow.addClass("row time-block");
-    divElRow.attr('data-hour', i);
-    divElHour.addClass('col-2 hour')
-    divElHour.text(hourFormat);
+//initial 
+function initialize() {
+
+    //timeblock creation loop
+    for (let i = 9; i < 18; i++) {
+        var timeblockHour = moment().startOf('day').hour(i)
+        var hourFormat = moment(timeblockHour).format('h:mm a')
+        var divElRow = $('<div>');
+        var divElHour = $('<div>')
+        var divElText = $('<div>');
+        var divElSave = $('<div>');
+        divElRow.addClass("row time-block");
+        divElRow.attr('data-hour', i);
+        divElHour.addClass('col-2 hour')
+        divElHour.text(hourFormat);
 
 
-    //div formatting for past present future
-    if (currHour.isAfter(timeblockHour)) {
-        console.log('past')
-        divElRow.addClass('past')
-    } else if (currHour.isSame(timeblockHour)) {
-        console.log('present')
-        divElRow.addClass('present')
-    } else {
-        console.log('future')
-        divElRow.addClass('future')
+        //div formatting for past present future
+        if (currHour.isAfter(timeblockHour)) {
+            console.log('past')
+            divElRow.addClass('past')
+        } else if (currHour.isSame(timeblockHour)) {
+            console.log('present')
+            divElRow.addClass('present')
+        } else {
+            console.log('future')
+            divElRow.addClass('future')
+        }
+
+        divElText.addClass('col-9 description')
+        divElSave.addClass('col-1 saveBtn');
+
+        //dom appending
+        container.append(divElRow);
+        divElRow.append(divElHour);
+        divElRow.append(divElText);
+        divElRow.append(divElSave);
+        divElSave.append("<img class='img-fluid ' src='assets/img/saveIcon.png' alt='Png File Svg - Save Icon In Png Clipart@pikpng.com'></p>");
     }
-
-    divElText.addClass('col-9 description')
-    divElSave.addClass('col-1 saveBtn');
-
-    //dom appending
-    container.append(divElRow);
-    divElRow.append(divElHour);
-    divElRow.append(divElText);
-    divElRow.append(divElSave);
-    divElSave.append("<img src='assets/img/saveIcon.png'></p>");
 }
+
+initialize();
+
+//onclick on description
+//editable if future else not
+//onclick on savebutton
+//save pressed, load into json
+
+//add json parse on the initialize
