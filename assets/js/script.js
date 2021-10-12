a36e8502622864e21f7f04a4f4d4aab6279e997a
Cosdaman <Cosdaguy@gmail.com>
2021-10-12 11:25:03 -0700

    fixed event area

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,32 +15,34 @@ function initialize() {
 
     //timeblock creation loop
     for (let i = 9; i < 18; i++) {
+        //dom creation and manipulation
         var timeblockHour = moment().startOf('day').hour(i)
         var hourFormat = moment(timeblockHour).format('h:mm a')
         var divElRow = $('<div>');
         var divElHour = $('<div>')
-        var divElText = $('<div>');
+        var divElText = $('<textarea>');
         var divElSave = $('<div>');
         divElRow.addClass("row time-block");
         divElRow.attr('data-hour', i);
         divElHour.addClass('col-2 hour')
         divElHour.text(hourFormat);
+        divElText.addClass('col-9 description')
+        divElSave.addClass('col-1 saveBtn');
 
 
         //div formatting for past present future
         if (currHour.isAfter(timeblockHour)) {
             console.log('past')
             divElRow.addClass('past')
+            divElText.prop('readonly',true);
         } else if (currHour.isSame(timeblockHour)) {
             console.log('present')
             divElRow.addClass('present')
+            divElText.prop('readonly',true);
         } else {
             console.log('future')
             divElRow.addClass('future')
         }
-
-        divElText.addClass('col-9 description')
-        divElSave.addClass('col-1 saveBtn');
 
         //dom appending
         container.append(divElRow);
@@ -53,9 +55,9 @@ function initialize() {
 
 initialize();
 
-//onclick on description
-//editable if future else not
+
 //onclick on savebutton
 //save pressed, load into json
+//on initialize, check if date is correct
 
 //add json parse on the initialize
